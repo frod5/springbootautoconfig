7c0240c2d1fd7a692f9fef57e9fa95f1401f7195
sunwooKim <frod5@kakao.com>
2023-04-28 15:40:54 +0900

    스프링 부트 - 핵심 원리와 활용: @Conditional 다양한 기능

--- src/main/java/hello/config/MemoryConfig.java
+++ src/main/java/hello/config/MemoryConfig.java
@@ -3,12 +3,14 @@ package hello.config;
 import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class)
+//@Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on")  //프로퍼티 키값이 memory이고 value값이 on인 경우, 이전에 만든 MemoryCondition와 같은 기능.
 public class MemoryConfig {
 
     @Bean
